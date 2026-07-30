ea5e5a8bd607e1e2561f83677bd458b137669f4f
wwew_honor <pasha20051344@gmail.com>
2026-07-30 20:29:24 +0400

    зависимости

--- next.config.ts
+++ next.config.ts
@@ -60,16 +60,6 @@ const nextConfig: NextConfig = {
           },
         ],
       },
-      {
-        // Кэширование JS/CSS
-        source: "/_next/static/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
     ];
   },
 
